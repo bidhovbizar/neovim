bed4cd0470c70cd2f94a77833423ef6f2872757d
Bidhov Bizar <bidhovbizar@gmail.com>
2025-07-23 16:50:09 +0000

    Attempt26: Removed vim-sleuth, depreciation ignored, lua diagnostic and autocompelete

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,5 +1,5 @@
 -- Install lua-language-server using the following command:
--- `npm install -g lua-language-server` probably 
+-- We can either install using Mason which I don't prefer or manually clone and build it and put in your $PATH
 return {
 	cmd = {
 		"lua-language-server",
@@ -17,13 +17,20 @@ return {
 		"selene.yml",
 		"stylua.toml",
 	},
-	-- settings = {
-	--     Lua = {
-	--         diagnostics = {
-	--             --     disable = { "missing-parameters", "missing-fields" },
-	--         },
-	--     },
-	-- },
+	settings = {
+		Lua = {
+			diagnostics = {
+				disable = { "missing-parameters", "missing-fields" },
+			},
+			format = {
+				enable = true,
+				defaultConfig = {
+					indent_style = "space",
+					indent_size = "4",
+				}
+			},
+		},
+	},
 
 	single_file_support = true,
 	log_level = vim.lsp.protocol.MessageType.Warning,

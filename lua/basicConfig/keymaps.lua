71388c47feb25799becada59f6c0bfd591f34e9a
bbizar <bbizar@cisco.com>
2025-07-29 04:08:00 +0530

    Setup avante with copilot, changed blink and conform. Fixed UI issues, but slower now, need to optimize and cleanup avante

--- lua/basicConfig/keymaps.lua
+++ lua/basicConfig/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Toggle relative numbers
 vim.keymap.set('n', '<leader>tr', '<cmd>set relativenumber!<cr>', { desc = "Toggle relative numbers" })
+
+-- Easier window resizing with arrow keys
+vim.keymap.set('n', '<M-Up>', ':resize +2<CR>', { silent = true })
+vim.keymap.set('n', '<M-Down>', ':resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<M-Left>', ':vertical resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<M-Right>', ':vertical resize +2<CR>', { silent = true })

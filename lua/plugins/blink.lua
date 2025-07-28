6bbd2440c0389279f87f9b0d89660ab8d44b6074
bbizar <bbizar@cisco.com>
2025-07-28 10:03:37 +0530

    Attempt 29: Setup avante with copilot, changed blink and conform. Slow now, need to optimize the use, still buggy

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,43 +1,81 @@
 return {
-    { "L3MON4D3/LuaSnip",
+    { 
+        "L3MON4D3/LuaSnip",
         event = "InsertEnter",
     },
-        {
+    {
+        -- Add blink-cmp-avante as a separate plugin that loads first
+        "Kaiser-Yang/blink-cmp-avante",
+        lazy = true, -- Changed: Let it load when needed by blink.cmp
+    },
+    {
         "saghen/blink.cmp",
         event = "InsertEnter",
         dependencies = {
             "rafamadriz/friendly-snippets",
+            "Kaiser-Yang/blink-cmp-avante", -- Dependency ensures proper load order
         },
         version = "*",
         config = function()
             require("blink.cmp").setup({
                 snippets = { preset = "luasnip" },
-                -- If an LSP server is running then signature will help you with the commands inside it
                 signature = { enabled = true },
-                -- If you have installed nerd-fonts in windows then you can use it to display icons else ignore this
-                --appearance = {
-                --    use_nvim_cmp_as_default = false,
-                --    nerd_font_variant = "normal",
-                --},
+
+                keymap = { preset = "default" },
+
                 sources = {
-                    -- If you want lsp to be the first source then you can use this
-                    default = { "lsp", "path", "snippets", "buffer" },
-                    -- If you don't want lsp but all the other features then go with this
-                    --default = { "path", "snippets", "buffer" },
+                    default = { "avante", "lsp", "path", "snippets", "buffer" },
+
+                    per_filetype = {
+                        -- Fixed: Correct filetype name (capital A)
+                        Avante = { "avante", "path", "buffer" },
+                        -- Also add lowercase variant just in case
+                        avante = { "avante", "path", "buffer" },
+                    },
+
                     providers = {
+                        path = {
+                            name = 'Path',
+                            module = 'blink.cmp.sources.path',
+                            score_offset = 3,
+                            opts = {
+                                trailing_slash = false,
+                                label_trailing_slash = true,
+                                get_cwd = function(context)
+                                    return vim.fn.expand(('#%d:p:h'):format(context.bufnr))
+                                end,
+                                show_hidden_files_by_default = false,
+                            }
+                        },
+
+                        avante = {
+                            module = 'blink-cmp-avante',
+                            name = 'Avante',
+                            score_offset = 1000, -- Added: High priority for @ and / triggers
+                            opts = {
+                                -- Options for blink-cmp-avante
+                            }
+                        },
+
                         cmdline = {
                             min_keyword_length = 2,
                         },
                     },
                 },
+
+                -- Fixed: Removed duplicate keymap section and consolidated
                 keymap = {
-                    ["<C-f>"] = {}, -- Ensure that we don't clash scroll up and down with documentation scroll.
+                    ["<C-f>"] = {}, -- Don't clash with documentation scroll
                     ["<Enter>"] = { "accept", "fallback" },
                     ["<C-y>"] = { "accept" },
                     ["<C-e>"] = { "cancel" },
                     ["<C-n>"] = { "select_next" },
                     ["<C-p>"] = { "select_prev" },
+                    -- Added: More navigation options
+                    ["<Tab>"] = { "select_next", "fallback" },
+                    ["<S-Tab>"] = { "select_prev", "fallback" },
                 },
+
                 cmdline = {
                     enabled = false,
                     completion = { menu = { auto_show = true } },
@@ -45,6 +83,7 @@ return {
                         ["<CR>"] = { "accept_and_enter", "fallback" },
                     },
                 },
+
                 completion = {
                     menu = {
                         border = nil,
@@ -53,12 +92,13 @@ return {
                         draw = {
                             columns = {
                                 { "kind_icon" },
-                                { "label",      "label_description", gap = 1 },
+                                { "label", "label_description", gap = 1 },
                                 { "kind" },
                                 { "source_name" },
                             },
                         },
                     },
+
                     documentation = {
                         window = {
                             border = nil,
@@ -69,6 +109,20 @@ return {
                         auto_show_delay_ms = 500,
                     },
                 },
+
+                -- Added: Enable completion in special buffer types (important for Avante)
+                enabled = function()
+                    local buftype = vim.api.nvim_get_option_value("buftype", { buf = 0 })
+                    local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
+
+                    -- Enable in Avante buffers specifically
+                    if filetype == "Avante" or filetype == "avante" then
+                        return true
+                    end
+
+                    -- Enable in normal files, disable in prompt/nofile buffers
+                    return not vim.tbl_contains({ "prompt" }, buftype) or buftype == ""
+                end,
             })
 
             require("luasnip.loaders.from_vscode").lazy_load()
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,20 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports", "golines", "gofmt" },
-			},
-			-- format_on_save = {
-			--     lsp_fallback = true,
-			--     async = false,
-			-- },
-		})
-	end,
+    "stevearc/conform.nvim",
+    lazy = true,
+    module = "conform",
+    cmd = { "ConformInfor", "ConformToggle", "ConformInfo" },
+    opts = {
+        formatters_by_ft = {},
+    },
+    --config = function()
+    --    --require("conform").setup({
+    --    --formatters_by_ft = {
+    --    --	lua = { "stylua" },
+    --    --	go = { "goimports", "golines", "gofmt" },
+    --    --},
+    --    -- format_on_save = {
+    --    --     lsp_fallback = true,
+    --    --     async = false,
+    --    -- },
+    --})
 }
